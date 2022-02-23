170c7e39bdb315ce800dccd1b29075b7ee1c03c0
mansour9897 <mansour9897@gmail.com>
2022-02-23 19:24:06 +0330

    merge source file to header file for moving avrage class

--- MovingAvrage.h
+++ MovingAvrage.h
@@ -7,6 +7,7 @@ private:
     unsigned int deg;
     float *buff;
     int index;
+
 public:
     MovingAvrage(unsigned int d);
     ~MovingAvrage();
@@ -15,7 +16,35 @@ public:
     float Avrage(void);
 };
 
+MovingAvrage::MovingAvrage(unsigned int d)
+{
+    this->deg = d;
+    this->buff = new float[this->deg];
+    this->index = 0;
+    // cout << "Created Filter...\r\n";
+}
 
+MovingAvrage::~MovingAvrage()
+{
+}
 
+void MovingAvrage::InserData(float data)
+{
+    buff[index++] = data;
+
+    if (!(index < this->deg))
+        index = 0;
+}
+
+float MovingAvrage::Avrage()
+{
+    float res = 0;
+    for (int i = 0; i < deg; i++)
+    {
+        res += buff[i];
+    }
+    res = res / deg;
+    return res;
+}
 
 #endif
